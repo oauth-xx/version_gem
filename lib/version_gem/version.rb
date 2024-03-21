51d7004b969b44a9f82b17e8d4af9112b2b5f15a
Peter Boling <peter.boling@gmail.com>
2024-03-21 15:01:10 -0600

    🔖 Prepare release v1.0.4

--- lib/version_gem/version.rb
+++ lib/version_gem/version.rb
@@ -2,7 +2,7 @@
 
 module VersionGem
   module Version
-    VERSION = "1.1.3"
+    VERSION = "1.1.4"
     # This would work in this gem, but not in external libraries,
     #   because version files are loaded in Gemspecs before bundler
     #   has a chance to load dependencies.
